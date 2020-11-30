7c67b515905337593d6ac638cffeaf46d2e1bea6
cxapython <1598828268@qq.com>
2020-12-01 00:05:44 +0800

    解码m3u8

--- dlm3u8.py
+++ dlm3u8.py
@@ -13,8 +13,10 @@ import re
 import json
 import click
 import glob
+from Crypto.Cipher import AES
 
-_temp = os.path.dirname(os.path.abspath(__file__))
+# _temp = os.path.dirname(os.path.abspath(__file__))
+_temp = "/Users/chennan/studymp4"
 m3u8_path = os.path.join(_temp, "m3u8")
 ts_path = os.path.join(_temp, "ts_file")
 mp4_path = os.path.join(_temp, "mp4_file")
@@ -25,12 +27,14 @@ if not os.path.exists(ts_path):
 if not os.path.exists(mp4_path):
     os.makedirs(mp4_path)
 session = requests.Session()
-MAX_WORKERS = 30  # 最多使用50个线程
+MAX_WORKERS = 20  # 最多使用50个线程
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36",
+}
 
 
-def download_from_url(url, dst, file_name):
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36"}
+def download_from_url(url, dst, file_name, aes_key=""):
     response = requests.get(url, headers=headers, stream=True, timeout=10)  # (1)
     file_size = int(response.headers['content-length'])  # (2)
     if os.path.exists(dst):
@@ -47,10 +51,16 @@ def download_from_url(url, dst, file_name):
         unit='B', unit_scale=True, desc=file_name)
     req = requests.get(url, headers=header, stream=True, timeout=10)  # (5)
     with(open(dst, 'ab')) as f:
-        for chunk in req.iter_content(chunk_size=1024):  # (6)
+        if aes_key:
+            chunk = decrypt(aes_key, req.content)
             if chunk:
                 f.write(chunk)
-                pbar.update(1024)
+                pbar.update(len(req.content))
+        else:
+            for chunk in req.iter_content(chunk_size=512):  # (6)
+                if chunk:
+                    f.write(chunk)
+                    pbar.update(512)
     pbar.close()
     return file_size
 
@@ -58,20 +68,44 @@ def download_from_url(url, dst, file_name):
 def get_ts_file(ts_item):
     url = ts_item[0]
     name = ts_item[1]
+    aes_key = ts_item[2]
     dst = os.path.join(ts_path, name)
-    download_from_url(url, dst, name)
+    download_from_url(url, dst, name, aes_key)
+
+
+def decrypt(key_str, content, iv_str="0" * 16):
+    """
+    AES解密
+    :return:
+    """
+    aes_key = bytes(key_str, encoding='utf-8')
+    aes_iv = bytes(iv_str, encoding='utf-8')
+    cipher = AES.new(aes_key, AES.MODE_CBC, aes_iv)
+    decrypt_bytes = cipher.decrypt(content)
+    return decrypt_bytes
 
 
 def read_file(prefix_url, file_path):
     with open(file_path, "r") as fs:
         index = 0
+        aes_key_str = ""
         for line in fs.readlines():
             new_item = line.strip().replace("\n", "")
+            if new_item.startswith("#EXT-X-KEY") and "METHOD=AES-128" in new_item:
+                # 目前只有aes128的解密
+                key_url = re.findall('URI="(.*?)"', new_item)[0]
+                cookies = {
+
+                }
+                req = requests.get(key_url, headers=headers, cookies=cookies)
+                if req.status_code in [200, 201]:
+                    aes_key_str = req.text
+                    print(f"获取密钥:{aes_key_str}")
             if new_item.endswith(".ts"):
                 if "/" in new_item:
                     new_item = new_item.split("/")[-1]
                 index += 1
-                yield f"{prefix_url}/{new_item}", f"{index}.ts"
+                yield f"{prefix_url}/{new_item}", f"{index}.ts", aes_key_str
 
 
 def get_m3u8(download_url, file_path):
@@ -102,6 +136,7 @@ def gen_mp4_file(file_name, text_path):
     retval = os.getcwd()
     print(f"当前目录:{retval}")
     command = f"ffmpeg -f concat -safe 0 -i '{text_path}' -c copy '{mp4_full_path}'"
+    print(command)
     try:
         completed = subprocess.run(command, check=True, shell=True,
                                    stdout=subprocess.PIPE)
@@ -119,8 +154,8 @@ def gen_mp4_file(file_name, text_path):
 
 
 def get_seed(video_id):
-    referer = ""
-    url_list = ["目标url"]
+    referer = f"https://jj.009xf.com/YaZhoux/{video_id}.html"
+    url_list = [f"https://jj.009xf.com/play/{video_id}/{index}/1.html" for index in range(1, 4)]
     for url in url_list:
         headers = {
             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36",
@@ -150,12 +185,12 @@ def main(video_id, input_url, name):
         m3u8_file_name = file_name = os.path.basename(url)
     elif video_id:
         url, file_name = get_seed(video_id)
-        m3u8_file_name = f'{file_name}.m3u8'
+        m3u8_file_name = file_name
     if name:
-        m3u8_file_name = name+".m3u8" if ".m3u8" not in name else name
+        m3u8_file_name = name
 
     prefix_url = os.path.dirname(url)
-    m3u8_file_path = os.path.join(m3u8_path, m3u8_file_name)
+    m3u8_file_path = os.path.join(m3u8_path, f"{m3u8_file_name}.m3u8")
     logger.info(f"file_name:{m3u8_file_name},url:{url}")
     get_m3u8(url, m3u8_file_path)
     ts_item_gen = read_file(prefix_url, m3u8_file_path)
@@ -163,7 +198,7 @@ def main(video_id, input_url, name):
         excutor.map(get_ts_file, ts_item_gen)
     logger.info("全部下载完成")
     logger.info("生成ts文件列表")
-    merge_text_path = merge_ts_file(file_name)
+    merge_text_path = merge_ts_file(m3u8_file_name)
     logger.info("生成mp4")
     gen_mp4_file(file_name, merge_text_path)
 
