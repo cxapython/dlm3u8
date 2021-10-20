ac9b65626a5dda4b1ffb4d2f251f9910f11b07e0
chenxiangan <1598828268@qq.com>
2021-10-20 16:05:39 +0800

    feat:downlaod error retry

--- README.md
+++ README.md
@@ -7,3 +7,9 @@
 #mac为例
 brew install ffmpeg
 ```
+
+
+#运行
+```
+python3.6 dlm3u8.py -n "xxx_class8" -p "https://video.xxx.com" -t 1 -l 1
+```
--- dlm3u8.py
+++ dlm3u8.py
@@ -3,20 +3,21 @@
 # @作者 : 陈祥安
 # @文件名 : dlmp4.py
 # @公众号: Python学习开发
-from concurrent.futures.thread import ThreadPoolExecutor
-from tqdm import tqdm
-import requests
-import os
-from loguru import logger
-import subprocess
-import re
 import json
-import click
-import glob
-from Crypto.Cipher import AES
+import os
+import re
+import subprocess
+from concurrent.futures.thread import ThreadPoolExecutor
 
-# _temp = os.path.dirname(os.path.abspath(__file__))
-_temp = "/Users/chennan/studymp4"
+import click
+import requests
+from Crypto.Cipher import AES
+from loguru import logger
+from retrying import retry
+from tqdm import tqdm
+
+_temp = os.path.dirname(os.path.abspath(__file__))
+# _temp = "/Users/chennan/studymp4"
 m3u8_path = os.path.join(_temp, "m3u8")
 ts_path = os.path.join(_temp, "ts_file")
 mp4_path = os.path.join(_temp, "mp4_file")
@@ -27,15 +28,22 @@ if not os.path.exists(ts_path):
 if not os.path.exists(mp4_path):
     os.makedirs(mp4_path)
 session = requests.Session()
-MAX_WORKERS = 20  # 最多使用50个线程
 
 headers = {
+    "referer": "https://www.kanxue.com/",
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36",
 }
 
 
-def download_from_url(url, dst, file_name, aes_key=""):
+@retry(stop_max_attempt_number=10, stop_max_delay=1000)
+def download_and_check(url, dst, file_name, my_code=None):
     response = requests.get(url, headers=headers, stream=True, timeout=10)  # (1)
+    if my_code is None:
+        my_code = [200, 201]
+    if response.status_code not in my_code:
+        logger.error(f"download error:{url}")
+        raise
+    logger.info(f"status_code:{response.status_code}")
     file_size = int(response.headers['content-length'])  # (2)
     if os.path.exists(dst):
         first_byte = os.path.getsize(dst)  # (3)
@@ -43,34 +51,52 @@ def download_from_url(url, dst, file_name, aes_key=""):
         first_byte = 0
     if first_byte >= file_size:  # (4)
         logger.info(f"{file_name}:下载完毕")
-        return file_size
-    logger.info(f"当前下载的是:{url}")
+        return first_byte, file_size, False
+    return first_byte, file_size, True
+
+
+@retry(stop_max_attempt_number=5, stop_max_delay=1000)
+def download_m3u8(url, dst, first_byte, file_size, file_name, aes_key="", aes_iv="0" * 16):
     header = {"Range": f"bytes={first_byte}-{file_size}"}
     pbar = tqdm(
         total=file_size, initial=first_byte,
         unit='B', unit_scale=True, desc=file_name)
+    header.update(headers)
     req = requests.get(url, headers=header, stream=True, timeout=10)  # (5)
-    with(open(dst, 'ab')) as f:
-        if aes_key:
-            chunk = decrypt(aes_key, req.content)
-            if chunk:
-                f.write(chunk)
-                pbar.update(len(req.content))
-        else:
-            for chunk in req.iter_content(chunk_size=512):  # (6)
+    if req.status_code in [200, 206]:
+        with(open(dst, 'ab')) as f:
+            if aes_key:
+                chunk = decrypt(aes_key, req.content, iv_str=aes_iv)
                 if chunk:
                     f.write(chunk)
-                    pbar.update(512)
+                    pbar.update(len(req.content))
+            else:
+                for chunk in req.iter_content(chunk_size=512):  # (6)
+                    if chunk:
+                        f.write(chunk)
+                        pbar.update(512)
+    else:
+        logger.error("下载失败")
+        raise
     pbar.close()
     return file_size
+
+
+def download_from_url(url, dst, file_name, aes_key="", aes_iv="", my_code=None):
+    first_byte, file_size, flag = download_and_check(url, dst, file_name, my_code)
+    if not flag:
+        return
+    logger.info(f"当前下载的是:{url}")
+    download_m3u8(url, dst, first_byte, file_size, file_name, aes_key, aes_iv)
 
 
 def get_ts_file(ts_item):
     url = ts_item[0]
     name = ts_item[1]
     aes_key = ts_item[2]
+    aes_iv = ts_item[3]
     dst = os.path.join(ts_path, name)
-    download_from_url(url, dst, name, aes_key)
+    download_from_url(url, dst, name, aes_key, aes_iv)
 
 
 def decrypt(key_str, content, iv_str="0" * 16):
@@ -89,23 +115,28 @@ def read_file(prefix_url, file_path):
     with open(file_path, "r") as fs:
         index = 0
         aes_key_str = ""
+        iv_str = "0" * 16
         for line in fs.readlines():
             new_item = line.strip().replace("\n", "")
             if new_item.startswith("#EXT-X-KEY") and "METHOD=AES-128" in new_item:
                 # 目前只有aes128的解密
-                key_url = re.findall('URI="(.*?)"', new_item)[0]
-                cookies = {
+                if not aes_key_str:
+                    key_url = re.findall('URI="(.*?)"', new_item)[0]
+                    cookies = {
 
-                }
-                req = requests.get(key_url, headers=headers, cookies=cookies)
-                if req.status_code in [200, 201]:
-                    aes_key_str = req.text
-                    print(f"获取密钥:{aes_key_str}")
+                    }
+                    req = requests.get(key_url, headers=headers, cookies=cookies)
+                    if req.status_code in [200, 201]:
+                        aes_key_str = req.text
+                        print(f"获取密钥:{aes_key_str}")
+
+                iv_str_arr = re.findall('IV=(.*)', new_item)
+                iv_str = iv_str_arr[0] if iv_str_arr else iv_str
             if new_item.endswith(".ts"):
                 if "/" in new_item:
                     new_item = new_item.split("/")[-1]
                 index += 1
-                yield f"{prefix_url}/{new_item}", f"{index}.ts", aes_key_str
+                yield f"{prefix_url}/{new_item}", f"{index}.ts", aes_key_str, iv_str.replace("0x", "")[:16]
 
 
 def get_m3u8(download_url, file_path):
@@ -154,7 +185,7 @@ def gen_mp4_file(file_name, text_path):
 
 
 def get_seed(video_id):
-    referer=""
+    referer = ""
     url_list = []
     for url in url_list:
         headers = {
@@ -179,23 +210,32 @@ def get_seed(video_id):
 @click.option('-i', '--video_id', type=str, help='fetch id')
 @click.option('-u', '--input_url', type=str, help='fetch url')
 @click.option('-n', '--name', type=str, help='file name')
-def main(video_id, input_url, name):
-    if input_url:
-        url = input_url
-        m3u8_file_name = file_name = os.path.basename(url)
-    elif video_id:
-        url, file_name = get_seed(video_id)
-        m3u8_file_name = file_name
-    if name:
+@click.option('-l', '--local', type=int, default=0, help='local m3u8 file,please make url complete')
+@click.option('-p', '--prefix_url', type=str, default="", help='prefix url')
+@click.option('-t', '--threads', type=int, default=2, help='max threads nums')
+def main(video_id, input_url, name, local, prefix_url, threads):
+    m3u8_file_path = m3u8_file_name = file_name = name
+    if not name.endswith("m3u8"):
+        m3u8_file_path = f"{name}.m3u8"
         m3u8_file_name = name
-
-    prefix_url = os.path.dirname(url)
-    m3u8_file_path = os.path.join(m3u8_path, f"{m3u8_file_name}.m3u8")
-    logger.info(f"file_name:{m3u8_file_name},url:{url}")
-    get_m3u8(url, m3u8_file_path)
+    if local == 0:
+        if input_url:
+            url = input_url
+            m3u8_file_name = file_name = os.path.basename(url)
+        elif video_id:
+            url, file_name = get_seed(video_id)
+            m3u8_file_name = file_name
+        prefix_url = os.path.dirname(url)
+        m3u8_file_path = os.path.join(m3u8_path, f"{m3u8_file_name}.m3u8")
+        logger.info(f"file_name:{m3u8_file_name},url:{url}")
+        get_m3u8(url, m3u8_file_path)
     ts_item_gen = read_file(prefix_url, m3u8_file_path)
-    with ThreadPoolExecutor(MAX_WORKERS) as excutor:
-        excutor.map(get_ts_file, ts_item_gen)
+    if threads == 1:
+        for item in ts_item_gen:
+            get_ts_file(item)
+    else:
+        with ThreadPoolExecutor(threads) as excutor:
+            excutor.map(get_ts_file, ts_item_gen)
     logger.info("全部下载完成")
     logger.info("生成ts文件列表")
     merge_text_path = merge_ts_file(m3u8_file_name)
